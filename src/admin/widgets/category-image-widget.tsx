f7a55379036095f8be9699fe3303a7aa4109ef24
avargas-cloud <a.vargas@ecopowertech.com>
2026-01-22 00:16:46 -0500

    fix: Use proper typing and empty fragment in category widget

--- src/admin/widgets/category-image-widget.tsx
+++ src/admin/widgets/category-image-widget.tsx
@@ -1,13 +1,13 @@
 import { defineWidgetConfig } from "@medusajs/admin-sdk"
 import { Container, Heading } from "@medusajs/ui"
-import { DetailWidgetProps } from "@medusajs/framework/types"
+import { DetailWidgetProps, AdminProductCategory } from "@medusajs/framework/types"
 
 // Widget to display category image in sidebar
-const CategoryImageWidget = ({ data }: DetailWidgetProps) => {
-    const category = data as any // AdminProductCategory type
-
-    if (!category?.thumbnail) {
-        return null // Don't show widget if no image
+const CategoryImageWidget = ({
+    data
+}: DetailWidgetProps<AdminProductCategory>) => {
+    if (!data?.thumbnail) {
+        return <></> // Don't show widget if no image
     }
 
     return (
@@ -15,8 +15,8 @@ const CategoryImageWidget = ({ data }: DetailWidgetProps) => {
             <div className="flex flex-col gap-4 px-6 py-4">
                 <Heading level="h2">Category Image</Heading>
                 <img
-                    src={category.thumbnail}
-                    alt={category.name}
+                    src={data.thumbnail}
+                    alt={data.name}
                     className="w-full rounded-lg border border-gray-200"
                 />
             </div>
