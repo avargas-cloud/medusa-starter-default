40bf19f3de053e3ed51b63627c2ce3bd9cf79e52
Alejo <a.vargas@ecopowertech.com>
2026-02-23 20:48:45 -0500

    push

---
+++ src/scripts/check-order.ts
@@ -0,0 +1,20 @@
+import { Pool } from "pg"
+
+async function run() {
+    process.env.DATABASE_URL = "postgresql://postgres:hUMSVtteMnqSBZSuSGUBivBooMdRoKtj@interchange.proxy.rlwy.net:34919/railway"
+    const pool = new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false }
+    })
+
+    try {
+        const items = await pool.query('SELECT unit_price, quantity, subtotal, tax_total, total FROM "order_item" WHERE order_id = (SELECT id FROM "order" WHERE display_id = 1020)')
+        console.table(items.rows)
+    } catch (e) {
+        console.error(e)
+    } finally {
+        await pool.end()
+    }
+}
+
+run()
